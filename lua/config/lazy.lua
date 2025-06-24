b5131735c5761152a8aceadddf8b4e6738eb2e62
mattykurlzz <mamontovvanya@yandex.ru>
2025-06-24 16:48:07 +0300

    FIX: resolve errors

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,7 +25,7 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    { import = "plugins" },
+    -- { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
--- lua/vim-keymaps.lua
+++ lua/vim-keymaps.lua
@@ -2,7 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
-vim.g.mapleader = "\"
+vim.g.mapleader = "\\"
 
 --vim.keymap.set("n", "K", vim.lsp.buf.hover, {}),
 --vim.keymap.set("n", "gp", vim.lsp.buf.definition, {}),
