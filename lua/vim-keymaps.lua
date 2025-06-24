a3b0f7c6c9736ba6cdab62afcd825ff4986c092e
mattykurlzz <mamontovvanya@yandex.ru>
2025-06-24 16:39:04 +0300

    initial commit

--- lua/vim-keymaps.lua
+++ lua/vim-keymaps.lua
@@ -2,9 +2,14 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
-vim.g.mapleader = " "
+vim.g.mapleader = "\"
 
-vim.keymap.set('n', '<leader>k', '<cmd>:wincmd k<CR>', {silent = true})
-vim.keymap.set('n', '<leader>j', '<cmd>:wincmd j<CR>', {silent = true})
-vim.keymap.set('n', '<leader>h', '<cmd>:wincmd h<CR>', {silent = true})
-vim.keymap.set('n', '<leader>l', '<cmd>:wincmd l<CR>', {silent = true})
+--vim.keymap.set("n", "K", vim.lsp.buf.hover, {}),
+--vim.keymap.set("n", "gp", vim.lsp.buf.definition, {}),
+--vim.keymap.set("n", "gd", vim.lsp.buf.declaration, {}),
+--vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {}),
+--
+--vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
+vim.keymap.set("", "<leader>fs", function()
+			vim.lsp.buf.format({ async = true })
+		end, { desc = "[F]ormat" })
