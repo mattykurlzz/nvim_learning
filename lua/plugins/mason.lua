8cc7012c2c0a891677a8856d068206e88b6c9db7
Ivan Mamontov <imamontov@NB-9558.corp.yadro.com>
2026-02-25 12:17:22 +0300

    fix double lsp load

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,46 +8,89 @@ return {
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            capabilities.offsetEncoding = { "utf-16" }
+
             require("mason-lspconfig").setup({
                 automatic_installation = true,
+                ensure_installed = { "lua_ls", "pyright", "clangd", "rust_analyzer", "jsonls" },
+                handlers = {
+                    -- Default for all lsp
+                    function(server_name)
+                        require("lspconfig")[server_name].setup({
+                            capabilities = capabilities,
+                        })
+                    end,
+
+                    -- clangd
+                    ["clangd"] = function()
+                        require("lspconfig").clangd.setup({
+                            capabilities = capabilities,
+                            cmd = {
+                                "clangd",
+                                "--clang-tidy",
+                                "--clang-tidy-checks=*",
+                                "--compile-commands-dir=" .. vim.fn.getcwd(),
+                                "--background-index",
+                                "--header-insertion=iwyu",
+                                "--completion-style=detailed",
+                                "--log=verbose",
+                            },
+                            init_options = {
+                                usePlaceholders = true,
+                                completeUnimported = true,
+                                clangdFileStatus = true,
+                            },
+                        })
+                    end,
+
+                    ["lua_ls"] = function()
+                        require("lspconfig").lua_ls.setup({
+                            capabilities = capabilities,
+                            settings = {
+                                Lua = {
+                                    diagnostics = { globals = { "vim" } },
+                                    workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+                                },
+                            },
+                        })
+                    end,
+
+                    ["pyright"] = function()
+                        require("lspconfig").pyright.setup({
+                            capabilities = capabilities,
+                        })
+                    end,
+
+                    ["rust_analyzer"] = function()
+                        require("lspconfig").rust_analyzer.setup({
+                            capabilities = capabilities,
+                        })
+                    end,
+
+                    ["jsonls"] = function()
+                        require("lspconfig").jsonls.setup({
+                            capabilities = capabilities,
+                        })
+                    end,
+                },
             })
         end,
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
-            local lspconfig = require("lspconfig") -- Import lspconfig
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-            -- Global LSP Keymaps (Moved outside of setup tables)
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gp", vim.lsp.buf.definition, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.declaration, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
             vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
 
-            -- You MUST call .setup({}) for each server to start it
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        diagnostics = { globals = { "vim" } },
-                        workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-                    },
-                },
-            })
-
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-            })
-
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-                cmd = { "clangd", "--compile-commands-dir=build", "--log=verbose" },
-            })
-
-            lspconfig.rust_analyzer.setup({ capabilities = capabilities })
-            lspconfig.jsonls.setup({ capabilities = capabilities })
+            local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+            for type, icon in pairs(signs) do
+                local hl = "DiagnosticSign" .. type
+                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+            end
         end,
     },
 }
